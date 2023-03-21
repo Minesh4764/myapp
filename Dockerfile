11e9c08d60221ab5986fa9607ef848dea22408df
minesh4764 <minesh2087@gmail.com>
2023-03-21 12:15:16 -0400

    checkingto drop my logs to spaces

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,26 @@
+# FROM python:3.9
+
+# RUN apt-get update && apt-get install -y cron
+
+# WORKDIR /app
+
+# COPY requirements.txt .
+# RUN pip install -r requirements.txt
+
+# COPY app.py /app/
+# COPY my-cron-job /etc/cron.d/
+
+# RUN chmod 0644 /etc/cron.d/my-cron-job
+# RUN touch /var/log/cron.log
+# USER root
+# RUN echo "*/1 * * * * root /usr/local/bin/python /app/app.py >> /var/log/cron.log 2>&1" > /etc/cron.d/my-cron-job
+# RUN chmod 0644 /etc/cron.d/my-cron-job
+# RUN crontab /etc/cron.d/my-cron-job
+
+# CMD cron && tail -f /var/log/cron.log
 FROM python:3.9
 
-RUN apt-get update && apt-get install -y cron
+RUN apt-get update && apt-get install -y cron s3cmd
 
 WORKDIR /app
 
@@ -13,7 +33,7 @@ COPY my-cron-job /etc/cron.d/
 RUN chmod 0644 /etc/cron.d/my-cron-job
 RUN touch /var/log/cron.log
 
-RUN echo "*/1 * * * * root /usr/local/bin/python /app/app.py >> /var/log/cron.log 2>&1" > /etc/cron.d/my-cron-job
+RUN echo "*/1 * * * * root /usr/local/bin/python /app/app.py >> /var/log/cron.log 2>&1 && s3cmd --access_key=DO00QTR2VTHEK2R9URFJ --secret_key=Qrv4BQUStDQCCQ0wR97WvS0TDtrHGyJwChEet11Cu70 --endpoint-url=https://testpipeline.nyc3.digitaloceanspaces.com put /var/log/cron.log s3://testpipeline/cron/$(date +'%Y-%m-%d-%H:%M:%S').log" > /etc/cron.d/my-cron-job
 RUN chmod 0644 /etc/cron.d/my-cron-job
 RUN crontab /etc/cron.d/my-cron-job
 
--- app.py
+++ app.py
@@ -3,11 +3,41 @@ import signal
 from datetime import datetime
 app = Flask(__name__)
 
+import logging
+import sys
+
+# Configure the logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create a formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Create a handler that writes to stdout
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+# Create a handler that writes to stderr
+stderr_handler = logging.StreamHandler(sys.stderr)
+stderr_handler.setLevel(logging.ERROR)
+stderr_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(stdout_handler)
+logger.addHandler(stderr_handler)
+
+# Use the logger to write log messages
+logger.info('Starting the app')
+
+# Your application code goes here
+
+logger.info('Application finished')
 @app.route("/")
 def home():
     print("thisis jsut a test")
     now = datetime.now()
-    return f"Hello, World! Minesh Patel {now}"
+    return f"Hello, World! Minesh Patel{now}"
 
 def shutdown_server(signum, frame):
     print('Received signal %s, shutting down server...' % signum)
@@ -18,4 +48,4 @@ def shutdown_server(signum, frame):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=82)
